cae115ad5d2ed06139bdee1993207639e3f5b9eb
RazukOrz <razukorz@gmail.com>
2023-12-09 14:47:02 -0800

    Add Hello World Project.

---
+++ hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d
@@ -0,0 +1,5 @@
+/home/razukorz/Projects/Rust/learn_rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5: src/main.rs
+
+/home/razukorz/Projects/Rust/learn_rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/razukorz/Projects/Rust/learn_rust/hello_world/target/debug/hello_world: /home/razukorz/Projects/Rust/learn_rust/hello_world/src/main.rs
