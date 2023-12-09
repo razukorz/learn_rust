e0ddffc4661f2e4d864c5fec831af17785c877ee
RazukOrz <razukorz@gmail.com>
2023-12-09 14:56:55 -0800

    Remove Build Targets from hello_world.

--- hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d
+++
@@ -1,5 +0,0 @@
-/home/razukorz/Projects/Rust/learn_rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5: src/main.rs
-
-/home/razukorz/Projects/Rust/learn_rust/hello_world/target/debug/deps/hello_world-8e894cd653037ef5.d: src/main.rs
-
-src/main.rs:
